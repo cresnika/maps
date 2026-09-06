f515e51b5c470e1e15c1ecddf99d193395c568e0
cresnika <119930270+cresnika@users.noreply.github.com>
2026-09-06 17:57:36 +0200

    Update update_pois.py

--- scripts/update_pois.py
+++ scripts/update_pois.py
@@ -71,18 +71,24 @@ REGIONS = [
 
 POI_TYPES = [
 
+    # {
+    #     "name": "Mountain Passes",
+    #     "type": "mountain_pass",
+    #     "output": "mountain_passes.json",
+    #     "query": None,
+    #     "road_filter": (
+    #         '^(motorway|trunk|primary|secondary|tertiary|'
+    #         'unclassified|residential|service)$'
+    #     ),
+    #     "tile_size": None,
+    # },
     {
-        "name": "Mountain Passes",
-        "type": "mountain_pass",
-        "output": "mountain_passes.json",
-        "query": None,
-        "road_filter": (
-            '^(motorway|trunk|primary|secondary|tertiary|'
-            'unclassified|residential|service)$'
-        ),
-        "tile_size": None,
+        "name": "Tankstellen",
+        "type": "fuel",
+        "output": "fuel.json",
+        "query": 'node["amenity"="fuel"]',
+        "tile_size": 2.0,
 #    },
-
 #    {
 #        "name": "Campingplaetze",
 #        "type": "campsite",
