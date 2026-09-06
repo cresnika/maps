8bc179e852ee2b06eb82ce926877b0b10ca6685a
cresnika <119930270+cresnika@users.noreply.github.com>
2026-09-06 18:00:51 +0200

    Update update_pois.py
    NUR Pässe und Tankstellen

--- scripts/update_pois.py
+++ scripts/update_pois.py
@@ -75,17 +75,17 @@ REGIONS = [
 
 POI_TYPES = [
 
-    # {
-    #     "name": "Mountain Passes",
-    #     "type": "mountain_pass",
-    #     "output": "mountain_passes.json",
-    #     "query": None,
-    #     "road_filter": (
-    #         '^(motorway|trunk|primary|secondary|tertiary|'
-    #         'unclassified|residential|service)$'
-    #     ),
-    #     "tile_size": None,
-    # },
+    {
+        "name": "Mountain Passes",
+        "type": "mountain_pass",
+        "output": "mountain_passes.json",
+        "query": None,
+        "road_filter": (
+            '^(motorway|trunk|primary|secondary|tertiary|'
+            'unclassified|residential|service)$'
+        ),
+        "tile_size": None,
+    },
     {
         "name": "Tankstellen",
         "type": "fuel",
